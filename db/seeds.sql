0d94e881eaf22577b47bdcdfd2233c437d26ebdc
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-08 08:21:30 -0600

    added employee seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,3 +13,13 @@ VALUES ('Sales Lead', 1, 100000),
     ('Accountant', 3, 125000),
     ('Legal Team Lead', 2, 250000),
     ('Lawyer', 2, 190000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
