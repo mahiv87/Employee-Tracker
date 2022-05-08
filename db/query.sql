d655fe27ba2165f955c73fa7021a5ef2dc004975
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-08 09:36:50 -0600

    add code to query.sql

---
+++ db/query.sql
@@ -0,0 +1,7 @@
+SELECT role.id AS id,
+    role.title AS title,
+    department.name AS department,
+    role.salary AS salary
+FROM role
+JOIN department ON role.department_id = department.id
+ORDER BY id ASC;
