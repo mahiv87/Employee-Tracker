b44bc1e97f932fb16050cbf8c8399f79fc308fdb
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-08 11:20:06 -0600

    added db connection, and department query table

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
-const table = cTable.getTable([
+const db = mysql.createConnection(
     {
-        col1: 'foo',
-        col2: 'bar'
-    }
-]);
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'employee_tracker_db'
+    },
+    console.log(`Connected to the employee_tracker_db`)
+);
 
-console.log(table);
-
-console.table([
-    {
-        col1: 'bar',
-        col2: 'foo'
-    }
-]);
+db.query(`SELECT * FROM department`, (err, results) => console.table(results));
