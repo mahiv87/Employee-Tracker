5ba2db5c2e682b25d003d5bd3b3b7426faa91308
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-08 07:57:21 -0600

    added department seed

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES ('Sales'),
+    ('Legal'),
+    ('Finance'),
+    ('Engineering');
+
+INSERT INTO role (title, department_id, salary)
+VALUES ('Sales Lead', 1, 100000),
+    ('Salesperson', 1, 80000),
+    ('Lead Engineer', 4, 150000),
+    ('Software Engineer', 4, 120000),
+    ('Account Manager', 3, 160000),
+    ('Accountant', 3, 125000),
+    ('Legal Team Lead', 2, 250000),
+    ('Lawyer', 2, 190000);
